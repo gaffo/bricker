81b08c5514ffaed885492da15ecaeb247acd0fa8
Mike Gaffney <mike@uberu.com>
2015-07-28 21:07:38 -0700

    Able to map single file to a bl listwq

--- lxf.go
+++ lxf.go
@@ -2,15 +2,115 @@ package bricker
 
 import (
 	"archive/zip"
+	"bufio"
 	"encoding/xml"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
+type PartMap struct {
+	ldd2bl map[string]string
+	bl2ldd map[string]string
+}
+
+func (self *PartMap) LDD2BL(ldd string) string {
+	return self.ldd2bl[ldd]
+}
+
+func ParsePartsMap(file string) (*PartMap, error) {
+	m := PartMap{
+		ldd2bl: make(map[string]string),
+		bl2ldd: make(map[string]string),
+	}
+	fmt.Println("Parsing Parts File", file)
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		// LLDNAME LDDID BLID
+		parts := strings.Split(scanner.Text(), "\t")
+		// lddname := parts[0]
+		lddid := parts[1]
+		blid := parts[2]
+		m.ldd2bl[lddid] = blid
+		m.bl2ldd[blid] = lddid
+		fmt.Printf("%s->%s\n", lddid, blid)
+	}
+
+	return &m, nil
+}
+
+type ColorMap struct {
+	ldd2bl map[string]string
+	bl2ldd map[string]string
+}
+
+func (self *ColorMap) LDD2BL(ldd string) string {
+	return self.ldd2bl[ldd]
+}
+
+func ParseColorMap(file string) (*ColorMap, error) {
+	m := ColorMap{
+		ldd2bl: make(map[string]string),
+		bl2ldd: make(map[string]string),
+	}
+
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		// LDDID LDDNAME BLID BLNAME HEX
+		parts := strings.Split(scanner.Text(), "\t")
+		lddid := parts[0]
+		// lddname := parts[1]
+		blid := parts[2]
+		// blname := parts[3]
+		// hex := parts[4]
+		m.ldd2bl[lddid] = blid
+		m.bl2ldd[blid] = lddid
+	}
+
+	return &m, nil
+}
+
+type BLPart struct {
+	ItemID   string
+	Color    string
+	Quantity int64
+}
+
 type LXFML struct {
 	Bricks []LXFBrick `xml:"Bricks>Brick"`
+}
+
+func (self *LXFML) ConvertWithSources(colors *ColorMap, parts *PartMap) []BLPart {
+	bl := make([]BLPart, 0, 1024)
+
+	for _, brick := range self.Bricks {
+		if len(brick.Parts) == 0 {
+			continue
+		}
+		part := brick.Parts[0]
+		design := part.DesignID
+		materials := part.Materials
+		materials = strings.Split(materials, ",")[0]
+		bl = append(bl, BLPart{
+			ItemID:   parts.LDD2BL(design),
+			Color:    colors.LDD2BL(materials),
+			Quantity: 1,
+		})
+	}
+
+	return bl
 }
 
 type LXFBrick struct {
@@ -47,7 +147,6 @@ func (self *LFXParser) Parse(file string) (*LXFML, error) {
 		if err != nil {
 			return nil, err
 		}
-		fmt.Printf(string(b))
 
 		lxfml := LXFML{}
 		err = xml.Unmarshal(b, &lxfml)
--- lxf_test.go
+++ lxf_test.go
@@ -25,3 +25,39 @@ func Test_ParseLXF(t *testing.T) {
 	assert.Equal(t, "90609", lxfml.Bricks[2].Parts[0].DesignID)
 	assert.Equal(t, "26", lxfml.Bricks[2].Parts[0].Materials)
 }
+
+func Test_ConvertSimple(t *testing.T) {
+	parser := bricker.LFXParser{}
+	lxfml, err := parser.Parse("simple.lxf")
+	assert.NoError(t, err)
+
+	colorMap, err := bricker.ParseColorMap("ldd_bl_colors.csv")
+	assert.NoError(t, err)
+	partsMap, err := bricker.ParsePartsMap("ldd_bl_parts.csv")
+	assert.NoError(t, err)
+
+	blParts := lxfml.ConvertWithSources(colorMap, partsMap)
+
+	assert.Equal(t, 3, len(blParts))
+	assert.Equal(t,
+		bricker.BLPart{
+			ItemID:   "99475",
+			Color:    "110",
+			Quantity: 1,
+		},
+		blParts[0])
+	assert.Equal(t,
+		bricker.BLPart{
+			ItemID:   "99476",
+			Color:    "5",
+			Quantity: 1,
+		},
+		blParts[1])
+	assert.Equal(t,
+		bricker.BLPart{
+			ItemID:   "99495",
+			Color:    "11",
+			Quantity: 1,
+		},
+		blParts[2])
+}
